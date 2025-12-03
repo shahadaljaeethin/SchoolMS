6956b10ece13ed509b092a3e14b72c139654e66f
shahadaljaeethin <shahadaljaeethin@gmail.com>
2025-12-03 15:18:43 +0300

    Initial commit

---
+++ src/main/java/com/example/SchoolMS/SchoolMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SchoolMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SchoolMS/SchoolMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SchoolMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
